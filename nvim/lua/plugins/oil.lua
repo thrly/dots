d4c2f74b0307a37014def5ceb4b2a0cdb4136387
thrly <o.thrly@gmail.com>
2026-01-02 13:43:48 +0000

    Add Oil to nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      default_file_explorer = false, -- keep Snacks / Neo-tree
+    },
+    view_options = {
+      show_hidden = false,
+    },
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+}
