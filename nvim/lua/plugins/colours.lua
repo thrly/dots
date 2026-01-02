7ad4c55f4d7bf868c12501f18b865aa99c4256b8
thrly <o.thrly@gmail.com>
2026-01-02 13:44:01 +0000

    nvim cleanup

--- nvim/lua/plugins/colours.lua
+++ nvim/lua/plugins/colours.lua
@@ -12,20 +12,20 @@ return {
     end,
   },
 
-  {
-    -- make tokyonight transparent
-    "folke/tokyonight.nvim",
-
-    lazy = true,
-    y = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
+  -- {
+  --   -- make tokyonight transparent
+  --   "folke/tokyonight.nvim",
+  --
+  --   lazy = true,
+  --   y = 1000, -- make sure to load this before all the other start plugins
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
   -- {
   --   dir = "/home/thrly/dev/petrichor.nvim", -- or wherever your repo is
   --   name = "petrichor",
@@ -49,6 +49,7 @@ return {
     opts = {
       -- colorscheme = "petrichor",
       -- colorscheme = "tokyonight-moon",
+      colorscheme = "solarized-osaka",
     },
   },
   {
