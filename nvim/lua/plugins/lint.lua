48a532fa9fd387ab5330ce0623dac1fd5f36a3bc
thrly <o.thrly@gmail.com>
2026-02-26 09:35:54 +0000

    remove markdown linting

---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,13 @@
+-- using this to disable markdown linting errors by default
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", "~/.markdownlint-cli2.jsonc", "--" },
+        },
+      },
+    },
+  },
+}
